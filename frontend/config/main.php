341d18e61350f3267974c9245a96c0e2baba1e59
ZillDing <zilld@hotmail.com>
2013-01-12 15:23:03 +0800

    add twitter bootstrap module

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -35,7 +35,10 @@ return CMap::mergeArray(
 		'params' => $params,
 		// preload components required before running applications
 		// @see http://www.yiiframework.com/doc/api/1.1/CModule#preload-detail
-		'preload' => array('log'),
+		'preload' => array(
+			'log',
+			'bootstrap',
+		),
 		// @see http://www.yiiframework.com/doc/api/1.1/CApplication#language-detail
 		'language' => 'en',
 		// uncomment if a theme is used
@@ -55,13 +58,22 @@ return CMap::mergeArray(
 			'application.controllers.*',
 			'application.models.*'
 		),
-		/* uncomment and set if required */
 		// @see http://www.yiiframework.com/doc/api/1.1/CModule#setModules-detail
-		/* 'modules' => array(), */
+		'modules' => array(
+			'gii' => array(
+				'generatePaths' => array(
+					'bootstrap.gii'
+				),
+			),
+		),
 		'components' => array(
 			'errorHandler' => array(
 				// @see http://www.yiiframework.com/doc/api/1.1/CErrorHandler#errorAction-detail
 				'errorAction'=>'site/error'
+			),
+			'bootstrap' => array(
+				'class' => 'common.extensions.bootstrap.components.Bootstrap',
+				'responsiveCss' => true,
 			),
 			'db' => array(
 				'connectionString' => $params['db.connectionString'],
