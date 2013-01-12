2e7a80e125b2782d3470603cd6ca2c64402448d0
ZillDing <zilld@hotmail.com>
2013-01-12 16:59:36 +0800

    fix style bug + finish login form style

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -19,13 +19,12 @@
 	<!-- Use the .htaccess and remove these lines to avoid edge case issues. More info: h5bp.com/b/378 -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-	<title><?php echo h($this->pageTitle); /* using shortcut for CHtml::encode */ ?></title>
 	<meta name="description" content="">
 	<meta name="keywords" content="">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 
-	<!-- <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css"> -->
-	<!-- <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/styles.css"/> -->
+	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/styles.css"/>
 	<!--using less instead? file not included-->
 	<!--<link rel="stylesheet/less" type="text/css" href="/less/styles.less">-->
 
@@ -43,6 +42,7 @@
 
 	<!--<script src="/less/less-1.3.0.min.js"></script>-->
 	<link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico">
+	<title><?php echo h($this->pageTitle); /* using shortcut for CHtml::encode */ ?></title>
 </head>
 
 <body>
--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -14,42 +14,24 @@ $this->breadcrumbs=array(
 <p>Please fill out the following form with your login credentials:</p>
 
 <div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
+	<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+		'id'=>'login-form',
+		'enableClientValidation'=>true,
+		'htmlOptions'=>array('class'=>'well'),
+		'clientOptions'=>array(
+			'validateOnSubmit'=>true,
+		),
+	)); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+	<?php echo $form->textFieldRow($model, 'username', array('class'=>'span3'));?>
+	<?php echo $form->passwordFieldRow($model, 'password', array('class'=>'span3'));?>
+	<?php echo $form->checkBoxRow($model, 'rememberMe');?>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?> 
-		<!-- 
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
-		-->	
+	<div class="form-actions">
+		<?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit','type'=>'primary','label'=>'Login', 'icon'=>'ok'));?>
+		<?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'reset','label'=>'Reset'));?>
 	</div>
-
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
+	<?php $this->endWidget(); ?>
 </div><!-- form -->
