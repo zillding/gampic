47c0c24c385ad12fd057a5c89c4a69f7e10b0c8b
ZillDing <zilld@hotmail.com>
2013-01-14 14:40:23 +0800

    enable client side validation for upload form

--- frontend/views/site/upload.php
+++ frontend/views/site/upload.php
@@ -17,8 +17,11 @@ $this->breadcrumbs=array(
 
 	<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
 		'id'=>'upload-form',
-		'enableAjaxValidation'=>false,
+		'enableClientValidation'=>true,
 		'htmlOptions'=>array('class' => 'well', 'enctype'=>'multipart/form-data'),
+		'clientOptions'=>array(
+			'validateOnSubmit'=>true,
+		),
 	)); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
@@ -26,7 +29,7 @@ $this->breadcrumbs=array(
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class='control-group'>
-	<?php echo $form->fileFieldRow($model,'image', array()); ?>
+	<?php echo $form->fileFieldRow($model,'image', array()); // todo: css need adjusting ?>
 	</div>
 	<div class='control-group'>
 	<?php echo $form->textFieldRow($model,'image_title'); ?>
