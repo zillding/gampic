8592385ed6dd4b2f22488cc8d9c6385e071efb4c
ZillDing <zilld@hotmail.com>
2013-01-20 09:22:25 +0800

    update jquery.min.js to newest version

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -117,11 +117,13 @@
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/plugins.js"></script>
 <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/main.js"></script>
 <!-- Google Analytics -->
+<!--
 <script>
 	var _gaq=[['_setAccount','<?php echo param('google.analytics.account'); // check global.php shortcut file at "common/lib/" ?>'],['_trackPageview']];
 	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 		s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
+-->
 </body>
 </html>
