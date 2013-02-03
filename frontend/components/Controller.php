61bfc240989f7435c02573da8cccd4a46cb5c891
ZillDing <zilld@hotmail.com>
2013-02-03 14:52:10 +0800

    add dropdown on hover feature

--- frontend/components/Controller.php
+++ frontend/components/Controller.php
@@ -15,6 +15,9 @@ class Controller extends CController {
 	{
 		// include some js which is globally shared across the site
 		Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/scroller.js', CClientScript::POS_END);
+		// to make the dropdown list show on hover
+		Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/twitter-bootstrap-hover-dropdown.min.js', CClientScript::POS_END);
+		Yii::app()->clientScript->registerScript('dropdownOnHover', '$(function() {$(".dropdown-toggle").dropdownHover();})', CClientScript::POS_END);
 		parent::init();
 	}
 
